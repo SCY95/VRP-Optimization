b8de96153fbff00d87840c64eb224cfc911d0892
SCY95 <canyilmaz999@gmail.com>
2019-07-24 09:51:12 +0300

    locationtest

--- Struct/LocationDB.cs
+++ Struct/LocationDB.cs
@@ -98,156 +98,156 @@ namespace VrpTest.Struct
                 "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         }
 
-        //public static void GetCustomLocations()
-        //{
-        //    //Default Locations
-        //    Location location = new Location("35.051031+-89.793120", 0, 200, true);//depot -- 3610+Hacks+Cross+Rd+Memphis+TN
-        //    Locations.Add(location);
+        public static void GetCustomLocations()
+        {
+            //Default Locations
+            Location location = new Location(0,"", new Position(35.051031, -89.793120), 0, 200, true);//depot -- 3610+Hacks+Cross+Rd+Memphis+TN
+            Locations.Add(location);
 
-        //    Location location1 = new Location("Piperton+Tennessee", 40, 56, false);
-        //    Locations.Add(location1);
+            Location location1 = new Location(0,"", new Position(35.045108, -89.621660), 40, 56, false);
+            Locations.Add(location1);
 
-        //    Location location2 = new Location("205+Windbrook+Dr+Collierville+TN", 30, 45, false);
-        //    Locations.Add(location2);
+            Location location2 = new Location(0,"", new Position(35.049904, -89.614416), 30, 45, false);
+            Locations.Add(location2);
 
-        //    Location location3 = new Location("55+Ballard+Rd+Collierville+TN", 55, 70, false);
-        //    Locations.Add(location3);
+            Location location3 = new Location(0,"", new Position(35.045100, -89.610934), 55, 70, false);
+            Locations.Add(location3);
 
-        //    Location location4 = new Location("1532+Madison+Ave+Memphis+TN", 65, 88, false);
-        //    Locations.Add(location4);
+            Location location4 = new Location(0,"", new Position(35.138350, -90.010171), 65, 88, false);
+            Locations.Add(location4);
 
-        //    Location location5 = new Location("Pleasant+Hill+Mississippi+38654", 40, 65, false);
-        //    Locations.Add(location5);
+            Location location5 = new Location("Pleasant+Hill+Mississippi+38654", 40, 65, false);
+            Locations.Add(location5);
 
-        //    Location location6 = new Location("3641+Central+Ave+Memphis+TN", 25, 50, false);
-        //    Locations.Add(location6);
+            Location location6 = new Location("3641+Central+Ave+Memphis+TN", 25, 50, false);
+            Locations.Add(location6);
 
-        //    Location location7 = new Location("Lewisburg+Mississippi+38654", 0, 35, false);
-        //    Locations.Add(location7);
+            Location location7 = new Location("Lewisburg+Mississippi+38654", 0, 35, false);
+            Locations.Add(location7);
 
-        //    Location location8 = new Location("4339+Park+Ave+Memphis+TN", 25, 54, false);
-        //    Locations.Add(location8);
+            Location location8 = new Location("4339+Park+Ave+Memphis+TN", 25, 54, false);
+            Locations.Add(location8);
 
-        //    Location location9 = new Location("600+Goodwyn+St+Memphis+TN", 30, 55, false);
-        //    Locations.Add(location9);
+            Location location9 = new Location("600+Goodwyn+St+Memphis+TN", 30, 55, false);
+            Locations.Add(location9);
 
-        //    Location location10 = new Location("2000+North+Pkwy+Memphis+TN", 100, 120, false);
-        //    Locations.Add(location10);
+            Location location10 = new Location("2000+North+Pkwy+Memphis+TN", 100, 120, false);
+            Locations.Add(location10);
 
-        //    Location location11 = new Location("262+Danny+Thomas+Pl+Memphis+TN", 50, 65, false);
-        //    Locations.Add(location11);
+            Location location11 = new Location("262+Danny+Thomas+Pl+Memphis+TN", 50, 65, false);
+            Locations.Add(location11);
 
-        //    Location location12 = new Location("125+N+Front+St+Memphis+TN", 65, 80, false);
-        //    Locations.Add(location12);
+            Location location12 = new Location("125+N+Front+St+Memphis+TN", 65, 80, false);
+            Locations.Add(location12);
 
-        //    Location location13 = new Location("5959+Park+Ave+Memphis+TN", 15, 35, false);
-        //    Locations.Add(location13);
+            Location location13 = new Location("5959+Park+Ave+Memphis+TN", 15, 35, false);
+            Locations.Add(location13);
 
-        //    Location location14 = new Location("814+Scott+St+Memphis+TN", 45, 80, false);
-        //    Locations.Add(location14);
+            Location location14 = new Location("814+Scott+St+Memphis+TN", 45, 80, false);
+            Locations.Add(location14);
 
-        //    Location location15 = new Location("1005+Tillman+St+Memphis+TN", 55, 100, false);
-        //    Locations.Add(location15);
-
-
-        //    //Istanbul
-        //    //Not gunluk 600 dakika calisabilir
-
-        //    Location location16 = new Location("40.797142+29.382287", 0, 200, true);
-        //    Locations.Add(location16);//40.797142, 29.382287 Gebze Depo
-
-        //    Location location17 = new Location("40.879389+29.257468", 0, 80, false);
-        //    Locations.Add(location17);//Pendik Bakkal1
-
-        //    Location location18 = new Location("40.891902+29.189942", 0, 120, false);
-        //    Locations.Add(location18);//Kartal Bakkal1 
-
-        //    Location location19 = new Location("40.892085+29.193903", 30, 120, false);
-        //    Locations.Add(location19);//Kartal Bakkal2
-
-        //    Location location20 = new Location("40.930321+29.156061", 0, 180, false);
-        //    Locations.Add(location20);//Gulsuyu Bakkal1
-
-        //    Location location21 = new Location("40.925076+29.135498", 60, 210, false);
-        //    Locations.Add(location21);//Baglarbasi Bakkal1
-
-        //    Location location22 = new Location("41.039699+29.104331", 360, 480, false);
-        //    Locations.Add(location22);//Umraniye Bakkal1 
-
-        //    Location location23 = new Location("41.030835+29.035284", 0, 180, false);
-        //    Locations.Add(location23);//Uskudar Bakkal1 
-
-        //    //Gebze tarafi
-
-        //    Location location24 = new Location("40.835618+29.301208", 0, 180, false);
-        //    Locations.Add(location24);//Tuzla Bim
-
-        //    Location location25 = new Location("40.824819+29.310304", 0, 180, false);
-        //    Locations.Add(location25);//Tuzla Carrefour
-
-        //    Location location26 = new Location("40.825654+29.319878", 0, 180, false);
-        //    Locations.Add(location26);//Tuzla Mopas
-
-        //    Location location27 = new Location("40.824750+29.372604", 0, 240, false);
-        //    Locations.Add(location27);//Cayirova Mopas
-
-        //    Location location28 = new Location("40.817789+29.371047", 0, 240, false);
-        //    Locations.Add(location28);//Cayirova Migros Jet 
-
-        //    Location location29 = new Location("40.813845+29.402778", 0, 240, false);
-        //    Locations.Add(location29);//Gevze Bizim Market
-
-        //    Location location30 = new Location("40.810741+29.403533", 0, 240, false);
-        //    Locations.Add(location30);//Gebze Akmar
-
-        //    Location location31 = new Location("40.776801+29.389061", 0, 240, false);
-        //    Locations.Add(location31);//Darica Akdeniz Market 
-
-        //    Location location32 = new Location("40.776448+29.353651", 0, 240, false);
-        //    Locations.Add(location32);//Darica Ozen Market 
-
-        //    Location location33 = new Location("40.759876+29.385730", 0, 240, false);
-        //    Locations.Add(location33);//Darica Bim 
+            Location location15 = new Location("1005+Tillman+St+Memphis+TN", 55, 100, false);
+            Locations.Add(location15);
 
 
-        //    //Avrupa yakası
-        //    Location location34 = new Location("41.009756+28.967856", 240, 400, false);
-        //    Locations.Add(location34);//Şaafi Kebap Salonu Eminönü
+            //    //Istanbul
+            //    //Not gunluk 600 dakika calisabilir
 
-        //    Location location35 = new Location("41.016102+28.973159", 240, 600, false);
-        //    Locations.Add(location35);//Hafız Mustafa Eminönü
+            //    Location location16 = new Location("40.797142+29.382287", 0, 200, true);
+            //    Locations.Add(location16);//40.797142, 29.382287 Gebze Depo
 
-        //    Location location36 = new Location("41.062989+28.990483", 0, 240, false);
-        //    Locations.Add(location36);//Günay Rest Şişli
+            //    Location location17 = new Location("40.879389+29.257468", 0, 80, false);
+            //    Locations.Add(location17);//Pendik Bakkal1
 
-        //    Location location37 = new Location("41.109442+29.018298", 500, 600, false);
-        //    Locations.Add(location37);//Plaza Cubes Sarıyer
+            //    Location location18 = new Location("40.891902+29.189942", 0, 120, false);
+            //    Locations.Add(location18);//Kartal Bakkal1 
 
-        //    Location location38 = new Location("41.228735+29.111791", 0, 600, false);
-        //    Locations.Add(location38);//Cafe Fener Rumelifeneri
+            //    Location location19 = new Location("40.892085+29.193903", 30, 120, false);
+            //    Locations.Add(location19);//Kartal Bakkal2
 
-        //    Location location39 = new Location("41.064852+28.841873", 0, 400, false);
-        //    Locations.Add(location39);//Batışehir Focaccia Bağcılar/Esenler
+            //    Location location20 = new Location("40.930321+29.156061", 0, 180, false);
+            //    Locations.Add(location20);//Gulsuyu Bakkal1
 
-        //    Location location40 = new Location("40.992084+28.834974", 0, 200, false);
-        //    Locations.Add(location40);//Nef Ataköy 22
+            //    Location location21 = new Location("40.925076+29.135498", 60, 210, false);
+            //    Locations.Add(location21);//Baglarbasi Bakkal1
 
-        //    Location location41 = new Location("40.971358+28.791196", 200, 360, false);
-        //    Locations.Add(location41);//Görkem Kilis Sofrası Bakırköy
+            //    Location location22 = new Location("41.039699+29.104331", 360, 480, false);
+            //    Locations.Add(location22);//Umraniye Bakkal1 
 
-        //    Location location42 = new Location("41.022248+28.773106", 350, 500, false);
-        //    Locations.Add(location42);//Köfteci onur küçükçekmece atatürk parkı arkası
+            //    Location location23 = new Location("41.030835+29.035284", 0, 180, false);
+            //    Locations.Add(location23);//Uskudar Bakkal1 
 
-        //    Location location43 = new Location("41.181198+28.745308", 0, 240, false);
-        //    Locations.Add(location43);//Dominos Pizza Arnavutköy
+            //    //Gebze tarafi
 
-        //    Location location44 = new Location("41.007845+28.716998", 240, 600, false);
-        //    Locations.Add(location44);//Safran Elite Mangalbaşı & Cafe Avcılar
+            //    Location location24 = new Location("40.835618+29.301208", 0, 180, false);
+            //    Locations.Add(location24);//Tuzla Bim
 
-        //    Location location45 = new Location("41.103315+28.882457", 180, 480, false);
-        //    Locations.Add(location45);//Bülbülen Cagkebabi
+            //    Location location25 = new Location("40.824819+29.310304", 0, 180, false);
+            //    Locations.Add(location25);//Tuzla Carrefour
 
-        //}
+            //    Location location26 = new Location("40.825654+29.319878", 0, 180, false);
+            //    Locations.Add(location26);//Tuzla Mopas
+
+            //    Location location27 = new Location("40.824750+29.372604", 0, 240, false);
+            //    Locations.Add(location27);//Cayirova Mopas
+
+            //    Location location28 = new Location("40.817789+29.371047", 0, 240, false);
+            //    Locations.Add(location28);//Cayirova Migros Jet 
+
+            //    Location location29 = new Location("40.813845+29.402778", 0, 240, false);
+            //    Locations.Add(location29);//Gevze Bizim Market
+
+            //    Location location30 = new Location("40.810741+29.403533", 0, 240, false);
+            //    Locations.Add(location30);//Gebze Akmar
+
+            //    Location location31 = new Location("40.776801+29.389061", 0, 240, false);
+            //    Locations.Add(location31);//Darica Akdeniz Market 
+
+            //    Location location32 = new Location("40.776448+29.353651", 0, 240, false);
+            //    Locations.Add(location32);//Darica Ozen Market 
+
+            //    Location location33 = new Location("40.759876+29.385730", 0, 240, false);
+            //    Locations.Add(location33);//Darica Bim 
+
+
+            //    //Avrupa yakası
+            //    Location location34 = new Location("41.009756+28.967856", 240, 400, false);
+            //    Locations.Add(location34);//Şaafi Kebap Salonu Eminönü
+
+            //    Location location35 = new Location("41.016102+28.973159", 240, 600, false);
+            //    Locations.Add(location35);//Hafız Mustafa Eminönü
+
+            //    Location location36 = new Location("41.062989+28.990483", 0, 240, false);
+            //    Locations.Add(location36);//Günay Rest Şişli
+
+            //    Location location37 = new Location("41.109442+29.018298", 500, 600, false);
+            //    Locations.Add(location37);//Plaza Cubes Sarıyer
+
+            //    Location location38 = new Location("41.228735+29.111791", 0, 600, false);
+            //    Locations.Add(location38);//Cafe Fener Rumelifeneri
+
+            //    Location location39 = new Location("41.064852+28.841873", 0, 400, false);
+            //    Locations.Add(location39);//Batışehir Focaccia Bağcılar/Esenler
+
+            //    Location location40 = new Location("40.992084+28.834974", 0, 200, false);
+            //    Locations.Add(location40);//Nef Ataköy 22
+
+            //    Location location41 = new Location("40.971358+28.791196", 200, 360, false);
+            //    Locations.Add(location41);//Görkem Kilis Sofrası Bakırköy
+
+            //    Location location42 = new Location("41.022248+28.773106", 350, 500, false);
+            //    Locations.Add(location42);//Köfteci onur küçükçekmece atatürk parkı arkası
+
+            //    Location location43 = new Location("41.181198+28.745308", 0, 240, false);
+            //    Locations.Add(location43);//Dominos Pizza Arnavutköy
+
+            //    Location location44 = new Location("41.007845+28.716998", 240, 600, false);
+            //    Locations.Add(location44);//Safran Elite Mangalbaşı & Cafe Avcılar
+
+            //    Location location45 = new Location("41.103315+28.882457", 180, 480, false);
+            //    Locations.Add(location45);//Bülbülen Cagkebabi
+
+        }
 
     }
 }
