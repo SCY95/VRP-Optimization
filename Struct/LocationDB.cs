fd5c006f1d2acd90ab2abbba67f24bd16118a59e
canberkileri <37962286+canberkileri@users.noreply.github.com>
2019-07-24 10:00:40 +0300

    Location changed for test.

--- Struct/LocationDB.cs
+++ Struct/LocationDB.cs
@@ -116,37 +116,37 @@ namespace VrpTest.Struct
             Location location4 = new Location(0,"", new Position(35.138350, -90.010171), 65, 88, false);
             Locations.Add(location4);
 
-            Location location5 = new Location("Pleasant+Hill+Mississippi+38654", 40, 65, false);
+            Location location5 = new Location(0,"",new Position(34.912475, -89.892589), 40, 65, false);
             Locations.Add(location5);
 
-            Location location6 = new Location("3641+Central+Ave+Memphis+TN", 25, 50, false);
+            Location location6 = new Location(0,"",new Position(35.122387, -89.941454), 25, 50, false);
             Locations.Add(location6);
 
-            Location location7 = new Location("Lewisburg+Mississippi+38654", 0, 35, false);
+            Location location7 = new Location(0,"",new Position(34.861102, -89.831365), 0, 35, false);
             Locations.Add(location7);
 
-            Location location8 = new Location("4339+Park+Ave+Memphis+TN", 25, 54, false);
+            Location location8 = new Location(0,"",new Position(35.106714, -89.916721), 25, 54, false);
             Locations.Add(location8);
 
-            Location location9 = new Location("600+Goodwyn+St+Memphis+TN", 30, 55, false);
+            Location location9 = new Location(0,"",new Position(35.116022, -89.962513), 30, 55, false);
             Locations.Add(location9);
 
-            Location location10 = new Location("2000+North+Pkwy+Memphis+TN", 100, 120, false);
+            Location location10 = new Location(0,"",new Position(35.153155, -89.992550), 100, 120, false);
             Locations.Add(location10);
 
-            Location location11 = new Location("262+Danny+Thomas+Pl+Memphis+TN", 50, 65, false);
+            Location location11 = new Location(0,"",new Position(35.153526, -90.043629), 50, 65, false);
             Locations.Add(location11);
 
-            Location location12 = new Location("125+N+Front+St+Memphis+TN", 65, 80, false);
+            Location location12 = new Location(0,"",new Position(35.148928, -90.053377), 65, 80, false);
             Locations.Add(location12);
 
-            Location location13 = new Location("5959+Park+Ave+Memphis+TN", 15, 35, false);
+            Location location13 = new Location(0,"",new Position(35.098665, -89.865847), 15, 35, false);
             Locations.Add(location13);
 
-            Location location14 = new Location("814+Scott+St+Memphis+TN", 45, 80, false);
+            Location location14 = new Location(0,"",new Position(35.155035, -89.966837), 45, 80, false);
             Locations.Add(location14);
 
-            Location location15 = new Location("1005+Tillman+St+Memphis+TN", 55, 100, false);
+            Location location15 = new Location(0,"",new Position(35.159642, -89.959872), 55, 100, false);
             Locations.Add(location15);
 
 
