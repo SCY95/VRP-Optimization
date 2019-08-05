2d4f683df8de1d77198a7284db87495a45075a92
canberkileri <37962286+canberkileri@users.noreply.github.com>
2019-08-05 10:15:30 +0300

    Code purity - 2.

--- VrpTest/SolveForDay.cs
+++ VrpTest/SolveForDay.cs
@@ -17,7 +17,6 @@ namespace VrpTest
         public static void SolveForAssignedDay(IDataInput dataInput, IDataOutput dataOutput, 
             VrpProblem vrpProblem, Day day,ConfigParams cfg, int[] VCMinMax)
         {
-
             //TimeMatrixInit(day, cfg);
 
             //SaveTimeMatrix(day);
@@ -29,17 +28,13 @@ namespace VrpTest
             int min = VCMinMax[0];
             int max = VCMinMax[1];
 
-            //try
-            //{
             while (day.LocationDropped && !day.InfeasibleNodes && max >= min)
             {
                 day.SetVehicleNumber(min);
                 day.ResetResults();
-
                 
                 vrpProblem.SolveVrpProblem(day, cfg, vrpProblem, dataOutput, VCMinMax);
-
-                    
+                  
                 dataOutput.PrintSolution(vrpProblem.day, vrpProblem.routing, vrpProblem.manager, vrpProblem.solution);
                 dataOutput.PrintStatus(vrpProblem.routing);
                 min++;
@@ -47,16 +42,7 @@ namespace VrpTest
             foreach (var item in day.DroppedLocations)
             {
                 day.Locations.Remove(item);
-            }
-                
-            //}
-            //catch
-            //{
-            //    //7.Güne geçerken Null Reference hatası vererek patlıyor.
-            //}
-
-
-
+            }               
 
             return;
         }
