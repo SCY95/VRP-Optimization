bd7f3e4fc2225cd5932fc840a4b2a4305abddf5f
canberkileri <37962286+canberkileri@users.noreply.github.com>
2019-08-05 09:37:30 +0300

    Code purity.

--- VrpTest/SolveForDay.cs
+++ VrpTest/SolveForDay.cs
@@ -17,7 +17,6 @@ namespace VrpTest
         public static void SolveForAssignedDay(IDataInput dataInput, IDataOutput dataOutput, 
             VrpProblem vrpProblem, Day day,ConfigParams cfg, int[] VCMinMax)
         {
-
             //TimeMatrixInit(day, cfg);
 
             //SaveTimeMatrix(day);
@@ -29,17 +28,12 @@ namespace VrpTest
             int min = VCMinMax[0];
             int max = VCMinMax[1];
 
-            //try
-            //{
             while ((day.LocationDropped && !day.InfeasibleNodes) && max >= min)
             {
                     day.SetVehicleNumber(min);
                     day.ResetResults();
 
-                    Console.WriteLine("While ara");
-                    Console.WriteLine(min + " " + max);
                     vrpProblem.SolveVrpProblem(day, cfg, vrpProblem, dataOutput, VCMinMax);
-
                     
                     dataOutput.PrintSolution(vrpProblem.day, vrpProblem.routing, vrpProblem.manager, vrpProblem.solution);
                     dataOutput.PrintStatus(vrpProblem.routing);
@@ -50,15 +44,6 @@ namespace VrpTest
                     day.Locations.Remove(item);
                 }
                 
-            //}
-            //catch
-            //{
-            //    //7.Güne geçerken Null Reference hatası vererek patlıyor.
-            //}
-
-
-
-
             return;
         }
 
@@ -74,8 +59,6 @@ namespace VrpTest
                 day.ResetResults();
 
                 vrpProblem.SolveVrpProblem(day, cfg, vrpProblem, dataOutput, VCMinMax);
-
-
             }
             foreach (var item in day.DroppedLocations)
             {
